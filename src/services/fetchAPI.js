9e0461545cc6e05e45a62bc44c0d2b545b15bbe3
OlehBrov <o.dudnyk85@gmail.com>
2023-05-15 19:17:08 +0300

    Update fetchAPI.js

--- src/services/fetchAPI.js
+++ src/services/fetchAPI.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://644667f90431e885f0110f5f.mockapi.io';
+const BASE_URL = 'https://64625a244dca1a66134434dd.mockapi.io';
 
 export const fetchContacts = async () => {
   const allContacts = await fetch(`${BASE_URL}/contacts`);
